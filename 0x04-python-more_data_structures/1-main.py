78129f58af2d9d7c50c19991f482ea59504953fd
njogued <njogued@gmail.com>
2022-12-09 13:27:51 +0000

    1-main.py "Replit"

--- 0x04-python-more_data_structures/1-main.py
+++ 0x04-python-more_data_structures/1-main.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
-search_replace = __import__('1-search_replace').search_replace
+def search_replace(my_list, search, replace):
+  #new_new_list = []
+  #for item in my_list:
+   # if item == search:
+    #  new_new_list.append(replace)
+    #elif item != search:
+     # new_new_list.append(item)
+  #return(new_new_list)
+  return [replace if item == search else item for item in my_list]
 
 my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
 new_list = search_replace(my_list, 2, 89)
 
 print(new_list)
 print(my_list)
-
-def search_replace(my_list, search, replace):
-  
