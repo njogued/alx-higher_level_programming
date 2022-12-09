ec82de34dd5dc10b712f0c85aead42448da6da36
njogued <njogued@gmail.com>
2022-12-09 13:28:32 +0000

    3-main.py "Replit"

---
+++ 0x04-python-more_data_structures/3-main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+def common_elements(set_1, set_2):
+  set_3 = set_1.intersection(set_2)
+  return set_3
+  #return set_1 & set_2
+
+set_1 = { "Python", "C", "Javascript" }
+set_2 = { "Bash", "C", "Ruby", "Perl" }
+c_set = common_elements(set_1, set_2)
+print(sorted(list(c_set)))
