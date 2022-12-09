1dcd2b8a0cf22d30b754d3bcfb3538fdf333f411
njogued <njogued@gmail.com>
2022-12-09 13:28:40 +0000

    4-main.py "Replit"

---
+++ 0x04-python-more_data_structures/4-main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+def only_diff_elements(set_1, set_2):
+  ''''set_3 = set_1.symmetric_difference(set_2)
+  return set_3 '''
+  return set_1 ^ set_2
+
+set_1 = { "Python", "C", "Javascript" }
+set_2 = { "Bash", "C", "Ruby", "Perl" }
+od_set = only_diff_elements(set_1, set_2)
+print(sorted(list(od_set)))
