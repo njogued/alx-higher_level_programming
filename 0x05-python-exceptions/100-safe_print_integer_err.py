f0e0f5bfff5d0bd70715e54b9433c9e1a9e59128
njogued <njogued@gmail.com>
2022-12-19 07:13:41 -0800

    . "Advanced Task 100"

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,7 +3,11 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except (ValueError, TypeError) as message:
+    except ValueError as message:
         print("Exception: ", end="")
         print(message)
+        return False
+    except TypeError as text:
+        print("Exception: ", end="")
+        print(text)
         return False
