75f31f9b2ac4d11f5c68cf5b0167cd819997045b
njogued <njogued@gmail.com>
2022-12-20 11:59:25 -0800

    1-square.py "Corrected pycodestyle"

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 """Defines a square """
+
+
 class Square:
+
     """Square class"""
+
     def __init__(self, size):
         """Initiate square class
         Args: size - size of square define
