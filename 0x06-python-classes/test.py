a9166600160b23a38fb146b37110114ecdeb581d
njogued <njogued@gmail.com>
2023-01-02 10:09:16 +0000

    test.py "Worked"

--- 0x06-python-classes/test.py
+++ 0x06-python-classes/test.py
@@ -1,18 +1,27 @@
 #!/usr/bin/env python3
 
 class Playa:
-  def __init__(self, name, health):
+  def __init__(self, name="", p_health=0):
     self.name = name
-    self.health = health
+    self.health = p_health
 
   @property
   def health(self):
-    print(f"{self.name}'s current health is {self.health}")
-    return self.health
+    return self.__health
+
+  @property
+  def name(self):
+    return self.__name
 
   @health.setter
   def health(self, newhealth):
-    self.health = newhealth
+    self.__health = newhealth
+    return self.__health
+
+  @name.setter
+  def name(self, newname):
+    self.__name = newname
+    return self.__name
 
   def __str__(self):
     return "Player: " + self.name + "\n" + "Health: " + str(self.health)
@@ -21,8 +30,8 @@ def main():
   p_name1 = input("Player 1: ")
   p_name2 = input("Player 2: ")
 
-  player1 = Playa(str(p_name1), "100")
-  player2 = Playa(str(p_name2), "100")
+  player1 = Playa(str(p_name1), 100)
+  player2 = Playa(str(p_name2), 100)
 
   print(f"The current player, {player1.name} has a health of {player2.health}")
   print(f"The second player, {player2.name} has a health of {player2.health}")
