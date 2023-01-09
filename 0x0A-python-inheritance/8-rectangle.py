1682939ce00db4f6ae3e61c8c3afa675c848293c
njogued <njogued@gmail.com>
2023-01-09 10:32:11 -0800

    Pull

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,34 +1,15 @@
 #!/usr/bin/python3
-
-"""
-python3 -c 'print(__import__("my_module").__doc__)'
-"""
-
-
-class BaseGeometry:
-    """python3 -c 'print(__import__("my_module").MyClass.__doc__)'"""
-    def area(self):
-        """python3 -c 'print(__import__("my_module").my_function.__doc__)'
-        python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        Integer validator function
-        """
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+"""Inheris from baseGeometry"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """python3 -c 'print(__import__("my_module").MyClass.__doc__)'"""
+    """a class to define rectangle using BaseGeometry"""
+
     def __init__(self, width, height):
+        """Intialize a new Rectangle
         """
-        python3 -c 'print__(__import("my_module").my_function.__doc__)'"""
-        super().integer_validator("width", width)
+        self.integer_validator("width", width)
         self.__width = width
-        super().integer_validator("height", height)
-        self.__height = height
+        self.integer_validator("height", height)
+        self.__height = height 
