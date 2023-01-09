858c342b892056b8a5c0d04706cc2bb00776b411
njogued <njogued@gmail.com>
2023-01-09 10:46:34 -0800

    Area defined

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Inheris from baseGeometry"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """a class to define rectangle using BaseGeometry"""
+
+    def __init__(self, width, height):
+        """Intialize a new Rectangle
+        """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        hxw = self.__width * self.__height
+        return(hxw)
+
+    def __str__(self):
+        return("[Rectangle] " + str(self.__width) + "/" + str(self.__height))
