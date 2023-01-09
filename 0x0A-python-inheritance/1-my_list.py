1ceb81bb23cf2a864e4d35cf173ee3d9387b617a
njogued <njogued@gmail.com>
2023-01-09 03:09:38 -0800

    Pycodestyle

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,9 +6,5 @@ Function that inherits from list, sorts, and prints
 
 
 class MyList(list):
-    def __init__(self, iterable=[]):
-        """Constructor"""
-        super().__init__()
-
     def print_sorted(self):
         print(sorted(self))
