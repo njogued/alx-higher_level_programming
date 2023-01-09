be25e170f7bbc024bc3463b8a7e47c37aae5dff9
njogued <njogued@gmail.com>
2023-01-09 03:20:44 -0800

    Pycodestyle

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
 
 """
+python3 -c 'print(__import__("my_module").__doc__)')
 Function that inherits from list, sorts, and prints
 """
 
 
 class MyList(list):
+    """python3 -c 'print(__import__("my_module").MyClass.__doc__)'"""
     def print_sorted(self):
+        """python3 -c 'print(__import__("my_module").\
+            my_function.__doc__)'\
+            python3 -c 'print(__import__("my_module").\
+                MyClass.my_function.__doc__)')"""
         print(sorted(self))
