2d23bac837a12afadbba9c0609fb0fbb2b00ff78
njogued <njogued@gmail.com>
2023-01-09 10:58:50 -0800

    Square based on Rectangle

---
+++ 0x0A-python-inheritance/8.1-rectangle.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+"""
+python3 -c 'print(__import__("my_module").__doc__)'
+"""
+
+
+class BaseGeometry:
+    """python3 -c 'print(__import__("my_module").MyClass.__doc__)'"""
+    def area(self):
+        """python3 -c 'print(__import__("my_module").my_function.__doc__)'
+        python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Integer validator function
+        """
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """python3 -c 'print(__import__("my_module").MyClass.__doc__)'"""
+    def __init__(self, width, height):
+        """
+        python3 -c 'print__(__import("my_module").my_function.__doc__)'"""
+        super().integer_validator("width", width)
+        self.__width = width
+        super().integer_validator("height", height)
+        self.__height = height
