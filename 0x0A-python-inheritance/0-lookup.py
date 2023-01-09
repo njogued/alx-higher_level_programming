f6f22b66e10eef6f4e45bd8a764d86bef3dfe2bf
njogued <njogued@gmail.com>
2023-01-09 02:10:47 -0800

    Function that looks up object attributes and methods

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""
+Function to print the attributes and methods of an object
+"""
+
+
+def lookup(obj):
+    """ method to lookup class attributes
+    """
+    return [dir(obj)]
