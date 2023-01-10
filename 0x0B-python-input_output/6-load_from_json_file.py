2bebe6df9163188511fe878fea2d23181fb3d3fc
njogued <njogued@gmail.com>
2023-01-10 04:50:47 -0800

    return json.loads(f.read())

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -6,3 +6,6 @@ import json
 
 
 def load_from_json_file(filename):
+    """python3 -c 'print(__import__(Module).method.__doc__)'"""
+    with open(filename, "r", encoding="utf-8") as f:
+        return json.loads(f.read())
