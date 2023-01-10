429c09e2644461c9b614afa7af3d299bb56feae9
njogued <njogued@gmail.com>
2023-01-10 03:10:28 -0800

    READ FILE: with open() & print(f.read())

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,5 +6,6 @@ Function that reads a file
 
 
 def read_file(filename=""):
+    python3 -c 'print(__import__("my_module").my_function.__doc__)'
     with open(filename, "r") as file_1:
         print(file_1.read(), end="")
