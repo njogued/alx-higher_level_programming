2446acabda29d5118f5f20f3a89565133a1198c7
njogued <njogued@gmail.com>
2023-01-10 04:35:26 -0800

    f1.write(json.dumps(my_obj))

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -7,6 +7,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """python3 -c 'print(__import__(module).method.__doc__)'"""
-    json_obj = json.dumps(my_obj)
     with open(filename, "w") as f1:
-        f1.write(json_obj)
+        f1.write(json.dumps(my_obj))
