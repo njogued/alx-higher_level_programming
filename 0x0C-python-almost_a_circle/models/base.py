a0514e65d4286b77179ed72676fa4f568777a3dd
njogued <njogued@gmail.com>
2023-01-16 12:05:43 -0800

    save json string to file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,6 +3,8 @@
 '''
 Module with class and init methods
 '''
+import json
+
 
 class Base:
     """Base class"""
@@ -16,8 +18,20 @@ class Base:
 
         elif type(id) is int and id != None:
             self.id = id
+    
     def to_json_string(list_dictionaries):
-        if len(list_dictionares) == 0 or list_dictionaries is None:
+        if len(list_dictionaries) == 0 or list_dictionaries is None:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        filename = cls.__name__ + ".json"
+        result = []
+        if list_objs:
+            for i in list_objs:
+                result.append(cls.to_dictionary(i))
+
+        with open(filename, "w") as f:
+            f.write(cls.to_json_string(result))
