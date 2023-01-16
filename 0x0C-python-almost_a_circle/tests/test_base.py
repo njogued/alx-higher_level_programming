854562140fb9480a33c344d1b56adc3e93fde319
njogued <njogued@gmail.com>
2023-01-16 12:12:31 -0800

    Tests

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -1,25 +1,140 @@
 #!/usr/bin/python3
-
-"""
-Testing the base"""
 import unittest
-import sys
-sys.path.append("../models")
-Base = __import__('base').Base
+from models.base import Base
+from models.square import Square
+import json
+import inspect
+
+'''
+    Creating test cases for the base module
+'''
 
 
-class BaseTest(unittest.TestCase):
-    """Test class"""
+class test_base(unittest.TestCase):
+    '''
+        Testing base
+    '''
+    def test_id_none(self):
+        '''
+            Sending no id
+        '''
+        b = Base()
+        self.assertEqual(1, b.id)
+
     def test_id(self):
-        """test id function"""
-        b1 = Base(22)
-        b2 = Base()
-        b3 = Base(33)
-        b4 = Base()
-        self.assertEqual(b1.id, 22)
-        self.assertEqual(b2.id, 1)
-        self.assertEqual(b3.id, 33)
-        self.assertEqual(b4.id, 2)
+        '''
+            Sending a valid id
+        '''
+        b = Base(50)
+        self.assertEqual(50, b.id)
 
-if __name__ == "__main__":
-    unittest.main()
+    def test_id_zero(self):
+        '''
+            Sending an id 0
+        '''
+        b = Base(0)
+        self.assertEqual(0, b.id)
+
+    def test_id_negative(self):
+        '''
+            Sending a negative id
+        '''
+        b = Base(-20)
+        self.assertEqual(-20, b.id)
+
+    def test_id_string(self):
+        '''
+            Sending an id that is not an int
+        '''
+        b = Base("Betty")
+        self.assertEqual("Betty", b.id)
+
+    def test_id_list(self):
+        '''
+            Sending an id that is not an int
+        '''
+        b = Base([1, 2, 3])
+        self.assertEqual([1, 2, 3], b.id)
+
+    def test_id_dict(self):
+        '''
+            Sending an id that is not an int
+        '''
+        b = Base({"id": 109})
+        self.assertEqual({"id": 109}, b.id)
+
+    def test_id_tuple(self):
+        '''
+            Sending an id that is not an int
+        '''
+        b = Base((8,))
+        self.assertEqual((8,), b.id)
+
+    def test_to_json_type(self):
+        '''
+            Testing the json string
+        '''
+        sq = Square(1)
+        json_dict = sq.to_dictionary()
+        json_string = Base.to_json_string([json_dict])
+        self.assertEqual(type(json_string), str)
+
+    def test_to_json_value(self):
+        '''
+            Testing the json string
+        '''
+        sq = Square(1, 0, 0, 609)
+        json_dict = sq.to_dictionary()
+        json_string = Base.to_json_string([json_dict])
+        self.assertEqual(json.loads(json_string),
+                         [{"id": 609, "y": 0, "size": 1, "x": 0}])
+
+    def test_to_json_None(self):
+        '''
+            Testing the json string
+        '''
+        sq = Square(1, 0, 0, 609)
+        json_dict = sq.to_dictionary()
+        json_string = Base.to_json_string(None)
+        self.assertEqual(json_string, "[]")
+
+    def test_to_json_Empty(self):
+        '''
+            Testing the json string
+        '''
+        sq = Square(1, 0, 0, 609)
+        json_dict = sq.to_dictionary()
+        json_string = Base.to_json_string([])
+        self.assertEqual(json_string, "[]")
+
+
+class TestSquare(unittest.TestCase):
+    """
+    class for testing Base class' methods
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        """
+        Set up class method for the doc tests
+        """
+        cls.setup = inspect.getmembers(Base, inspect.isfunction)
+
+    def test_module_docstring(self):
+        """
+        Tests if module docstring documentation exist
+        """
+        self.assertTrue(len(Base.__doc__) >= 1)
+
+    def test_class_docstring(self):
+        """
+        Tests if class docstring documentation exist
+        """
+        self.assertTrue(len(Base.__doc__) >= 1)
+
+    def test_func_docstrings(self):
+        """
+        Tests if methods docstring documntation exist
+        """
+        for func in self.setup:
+            self.assertTrue(len(func[1].__doc__) >= 1)
