dd92ad9e9656731ca6045ad6232cef9970471db7
njogued <njogued@gmail.com>
2023-02-05 22:53:52 -0800

    BM

--- zz/simple_pg/basemodel.py
+++ zz/simple_pg/basemodel.py
@@ -1,24 +1,42 @@
 #!/usr/bin/python3
 '''
-Base model
+Base model for HBNB project
 '''
+from datetime import datetime
+from uuid import uuid4
+
 class BaseModel():
-    def __init__(self, name, mail):
-        self._name = name
-        self._mail = mail
-    
-    @property
-    def name(self):
-        return self._name
+    '''
+    Methods: save, to_dict, __str__, __init__
+    Attributes: id, created_at, updated_at
+    '''
+    def __init__(self):
+        '''
+        id - Unique ID
+        created_at - Time when object is instantiated
+        updated_at - Defaults to created_at when an object is instantiated
+        '''
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        '''
+        strftime("%A %d %B %Y at %H:%M:%S")
+        '''
 
-    @property
-    def mail(self):
-        return self._mail
+    def __str__(self):
+        '''
+        return a string representation of the class
+        '''
+        return f"[{__class__.__name__}] ({self.id}) {self.__dict__}"
 
-    @name.setter
-    def name(self, value):
-        self._name = value
+    def save(self):
+        '''
+        Update the updated_at instance attribute
+        '''
+        self.updated_at = datetime.now()
 
-    @mail.setter
-    def mail(self, value):
-        self._mail = value
+    def to_dict(self):
+        '''
+        returns a dict of key/value pairs of the instance
+        '''
+        sd = self.__dict__
