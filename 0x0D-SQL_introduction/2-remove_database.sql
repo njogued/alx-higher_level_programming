408bd8e86e69a1dde068a1e6f02e536895d0e2fc
njogued <njogued@gmail.com>
2023-02-15 08:33:49 -0800

    Dropping Databases like Flies

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,3 +1,3 @@
 -- Query to delete a database
 -- IF EXISTS flag ensures that the deletion does not fail if the database no longer exists
-DROP DATABASES IF NOT EXISTS hbtn_0c_0
+DROP DATABASE IF EXISTS hbtn_0c_0;
