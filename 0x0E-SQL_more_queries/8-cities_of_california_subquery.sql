2926218f3b4daf83635f745e1855738b4b2fc36b
njogued <njogued@gmail.com>
2023-02-16 13:30:24 -0800

    Cities of California

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- Select subquery Nested
-SELECT * FROM cities WHERE state_id IN (SELECT id FROM states WHERE name = 'California');
+SELECT id, name FROM cities WHERE state_id IN (SELECT id FROM states WHERE name = 'California') ORDER BY id ASC;
