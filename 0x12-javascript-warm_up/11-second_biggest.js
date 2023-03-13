8d7211793825eadf711561fbadf36427d9690189
njogued <njogued@gmail.com>
2023-03-13 12:58:05 -0700

    FF

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,10 +3,12 @@ if (process.argv.length < 4) {
   console.log(0);
 } else {
   let largest = 0;
+  const array = [];
   for (let x = 2; x < process.argv.length; x++) {
-    if (process.argv[x] > largest) {
+    if (process.argv[x] >= largest) {
       largest = process.argv[x];
+      array.unshift(largest);
     }
   }
-  console.log(largest);
+  console.log(array[1]);
 }
