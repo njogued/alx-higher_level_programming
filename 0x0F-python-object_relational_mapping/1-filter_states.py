f13e8c757b0dbae1e2864329997cd7c97cb1693b
njogued <njogued@gmail.com>
2023-03-17 14:09:54 -0700

    N%

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,13 +7,13 @@ try:
     usr = sys.argv[1]
     passwrd = sys.argv[2]
     dtbs = sys.argv[3]
-    db = MySQLdb.connect(host="localhost", user=usr, passwd=passwrd, db=dtbs)
-    cur = db.cursor
-    cur.execute("SELECT * FROM states WHERE name = 'Arizona'")
+    db = MySQLdb.connect(host="localhost", port=3306, user=usr, passwd=passwrd, db=dtbs)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC")
     rows = cur.fetchall()
     for row in rows:
         print(row)
     cur.close()
-    db,close()
+    db.close()
 except Exception as e:
     print(e)
