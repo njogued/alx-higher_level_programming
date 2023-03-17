0db60af702dd2c2fa2163ed14d63e4d76ae4e6e4
njogued <njogued@gmail.com>
2023-03-17 14:17:14 -0700

    Sweet home Arizona

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,7 +9,7 @@ try:
     search = sys.argv[4]
     db = MySQLdb.connect(host='localhost', port=3306, user=usr, passwd=passwrd, db=db_name)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE {} ORDER BY id".format(search))
+    cur.execute("SELECT * FROM states WHERE name = '{}' ORDER BY states.id".format(search))
     rows = cur.fetchall()
     for row in rows:
         print(row)
