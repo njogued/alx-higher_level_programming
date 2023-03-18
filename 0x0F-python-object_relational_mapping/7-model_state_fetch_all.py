094849b97b1925839667a3dc10a391919ac39d12
njogued <njogued@gmail.com>
2023-03-18 14:04:14 -0700

    SQLAlchemy Fetch

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+'''Session query'''
 
 import sys
-import sqlalchemy
+from sqlalchemy.orm import sessionmaker
 from model_state import State, Base
 from sqlalchemy import create_engine
 
@@ -9,5 +10,9 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     dtbs = sys.argv[3]
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}')
-
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format
+                           (username, password, dtbs))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for instance in session.query(State).order_by(State.id):
+        print("{0}: {1}".format(instance.id, instance.name))
