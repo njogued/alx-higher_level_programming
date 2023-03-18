936799589aaa9de58da1292b8c0f2fd0a4c15a38
njogued <njogued@gmail.com>
2023-03-18 12:57:53 -0700

    Filter cities

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+'''Search and Order'''
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    try:
+        usr = sys.argv[1]
+        passwrd = sys.argv[2]
+        db_name = sys.argv[3]
+        search = sys.argv[4]
+        db = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=usr,
+            passwd=passwrd,
+            db=db_name
+            )
+        cur = db.cursor()
+        q = "SELECT * FROM states WHERE BINARY name = %s \
+        ORDER BY states.id"
+        query = (q, (search,))
+        cur.execute(query)
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+        cur.close()
+        db.close()
+    except Exception as e:
+        print(e)
