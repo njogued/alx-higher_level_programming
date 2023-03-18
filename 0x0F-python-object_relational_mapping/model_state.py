90ea6dc6eb030bfc4eda130715f395b268751973
njogued <njogued@gmail.com>
2023-03-18 13:03:02 -0700

    Sqlalchemy begins

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
-
+'''Model base using sqlalchemy'''
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
+
+
 class State(Base):
+    '''cLASS State'''
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(128), nullable=False)
-    mysql_charset='latin1'
+    mysql_charset = 'latin1'
