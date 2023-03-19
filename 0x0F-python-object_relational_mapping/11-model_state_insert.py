ffd8f5dbe4e8ea85c9bbb0fb00380451b1f5ca98
njogued <njogued@gmail.com>
2023-03-19 01:33:50 -0700

    cREATE nEW mODEL sTATE: create new instance of State, call session.add(new instance) and session.commit() to update the db

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''Add new state to the database'''
+
+from model_state import Base, State
+from sqlalchemy import create_engine as ce
+from sqlalchemy.orm import sessionmaker as sm
+from sys import argv as v
+
+if __name__ == "__main__":
+    engine = ce(f'mysql+mysqldb://{v[1]}:{v[2]}@localhost:3306/{v[3]}')
+    Session = sm(bind=engine)
+    session = Session()
+    new = State(name="Louisiana")
+    session.add(new)
+    session.commit()
