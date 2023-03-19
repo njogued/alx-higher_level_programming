767598255169f9a18b12c1355e63a4eb65c6dd24
njogued <njogued@gmail.com>
2023-03-19 12:42:51 -0700

    Filter

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -12,12 +12,12 @@ if __name__ == "__main__":
     engine = create_engine(
         'mysql+mysqldb://{}:{}@localhost:3306/{}'
         .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-        pool_pre_ping=True
         )
     Session = sessionmaker(bind=engine)
     session = Session()
-    query = session.query(State, City).join(State)
+    query = session.query(State, City).filter(City.state_id == State.id).order_by(City.id)
     results = query.all()
     for state, city in results:
         print(f"{state.name}: ({city.id}) {city.name}")
+    session.commit()
     session.close()
