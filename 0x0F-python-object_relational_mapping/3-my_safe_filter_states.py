ca07f7c4e97231225435ebb76a2b6d7a706b6832
njogued <njogued@gmail.com>
2023-03-19 02:19:58 -0700

    MySQL injection safe

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,31 +1,31 @@
 #!/usr/bin/python3
-'''Search and Order'''
 
+'''
+lists all states in the database hbtn_0e_0_usa
+'''
 import sys
 import MySQLdb
 
+
 if __name__ == "__main__":
     try:
-        usr = sys.argv[1]
-        passwrd = sys.argv[2]
-        db_name = sys.argv[3]
-        search = sys.argv[4]
-        db = MySQLdb.connect(
-            host='localhost',
+        connection = MySQLdb.connect(
+            host="localhost",
+            user=sys.argv[1],
+            passwd=sys.argv[2],
             port=3306,
-            user=usr,
-            passwd=passwrd,
-            db=db_name
-            )
-        cur = db.cursor()
-        q = "SELECT * FROM states WHERE BINARY name = %s \
-        ORDER BY states.id"
-        query = (q, (search,))
-        cur.execute(query)
+            db=sys.argv[3]
+        )
+    except MySQLdb.Error:
+        print("connection error")
+    try:
+        cur = connection.cursor()
+        cur.execute("SELECT * FROM states WHERE name = %s ORDER BY\
+        states.id", (sys.argv[4],))
         rows = cur.fetchall()
         for row in rows:
             print(row)
-        cur.close()
-        db.close()
-    except Exception as e:
-        print(e)
+    except MySQLdb.Error:
+        print("Failed")
+    cur.close()
+    connection.close()
