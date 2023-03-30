953d10050a6c8b38e7f1702618447d8b35094f7e
njogued <njogued@gmail.com>
2023-03-30 14:17:36 -0700

    Peaking

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,6 +2,7 @@
 '''Find the peak'''
 
 def find_peak(list_of_integers):
+    '''Find the peak function'''
     if not list_of_integers:
         return None
     max_value = sorted(list_of_integers)
