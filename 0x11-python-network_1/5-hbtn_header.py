50f9bab967259c138ab1e1b5a44c319eff03c1ec
njogued <njogued@gmail.com>
2023-03-31 16:26:36 -0700

    Retrieve header from get request

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-'''Print the header in the output'''
-from requests import get
+"""retrieve a header from a get request"""
+
+import requests
 from sys import argv
 
 if __name__ == "__main__":
-    response = get(argv[1])
-    print(response.headers['X-Request-Id'])
+    response = requests.get(argv[1])
+    print(response.headers.get('X-Request-Id'))
