756dff91de3197db0fc195a4fc5a9586b0e10d70
njogued <njogued@gmail.com>
2023-04-25 14:31:38 -0700

    Writin to a file

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+// Using the fs module to write to a file
+const { writeFile } = require('fs');
+
+// The file as a command line argument
+const file = process.argv[2];
+
+// The text is the command line argument 3
+const text = process.argv[3];
+
+// Import the file system module
+// const fs = require('fs');
+
+writeFile(file, text, 'utf-8', (err) => {
+  if (err) throw err;
+  });
