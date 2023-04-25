42ca7c1fdbc5f3088a553116082202cc6ab9f5ec
njogued <njogued@gmail.com>
2023-04-25 16:13:50 -0700

    Getting the tasks for each user

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+// Print the number of completed tasks
+
+address = process.argv[2];
+
+const rq = require('request');
+let tasks = 0;
+
+// Declare a dict that will be updated with tasks and id values
+let dict = {};
+
+rq(address, (error, response, body) => {
+  if (error) throw error;
+  const output = JSON.parse(body);
+  for (let i = 1; i <= 10; i++) {
+    tasks = 0;
+    for (let j = 0; j <= output.length; j++) {
+      // output[j] prevents running out of index
+      if (output[j] && output[j].userId == i && output[j].completed == true)
+        tasks += 1;
+        };
+    dict[i] = tasks
+    // console.log(`'${i}': ${tasks}`)
+    };
+    console.log(dict);
+});
