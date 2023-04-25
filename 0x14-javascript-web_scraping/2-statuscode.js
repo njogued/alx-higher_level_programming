b43d4f7c801132ad9c6601a9243e6207ffe011bf
njogued <njogued@gmail.com>
2023-04-25 14:48:54 -0700

    Printing the status code using the requests module

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,13 +4,13 @@
 // request module is deprecated in newer node js versions
 // So dont take this too seriously
 
-address = process.argv[2];
+const address = process.argv[2];
 
 const re = require('request');
 
 re(address, (error, response, body) => {
   if (error) console.log(error);
-  process.stdout.write("code: ")
+  process.stdout.write('code: ');
   // console.log("code: ");
   console.log(response.statusCode);
-  })
+});
